a6f93db717f426eb4cac052bdd77b0eec3577355
Balram <balram.nitt@gmail.com>
2022-09-08 11:46:26 +0530

    changed the publicUrl to publicCaseUrl

--- src/Basename.js
+++ src/Basename.js
@@ -1,8 +1,13 @@
-export default function Basename({ currentLocation = window.location.href }, config) {
-    const container = `${config?.mfeContainerBasename}`
-    if (container) {
-        const url = currentLocation.includes(container) ? container : config?.publicUrl;
-        return url;
-    }
-    return config?.publicUrl ? config?.publicUrl : "model/case-inbox";
+export default function Basename(
+  { currentLocation = window.location.href },
+  config
+) {
+  const container = `${config?.mfeContainerBasename}`;
+  if (container) {
+    const url = currentLocation.includes(container)
+      ? container
+      : config?.publicCaseUrl;
+    return url;
+  }
+  return config?.publicCaseUrl ? config?.publicCaseUrl : "model/case-inbox";
 }
